d2f78db7d4bebc1a290248d6147d37964078150f
Shuhei1280245 <64823538+Shuhei1280245@users.noreply.github.com>
2020-06-10 11:40:16 +0900

    Add files via upload

---
+++ SingletonTest.java
@@ -0,0 +1,44 @@
+public class SingletonTest{
+    public static void main(String[] args){
+        Renban renbanA = Renban.getInstance();
+        Renban renbanB = Renban.getInstance();
+
+        System.out.println(renbanA.getNumber());
+        System.out.println(renbanB.getNumber());
+        System.out.println(renbanB.getNumber());
+        System.out.println(renbanA.getNumber());
+        System.out.println(renbanB.getNumber());
+
+    }
+
+}
+class Renban { 
+
+    private static Renban instance = new Renban();
+
+    private Renban(){
+        this.bangou = 0;
+
+        //外部からインスタンス化できないよう、private宣言
+    }
+
+    public static Renban getInstance() {
+        return instance;
+    }
+    public void renbanA(int bangou){
+        if (renbanA == renbanB)
+            renbanB += bangou;
+        else if (renbanA <= 9999)
+            renbanA += bangou;
+        else return;
+    }
+    public void renbanB(int bangou){
+        if (renbanA == renbanB)
+            renbanA += bangou;
+        else if (renbanB <= 9999)
+            renbanB += bangou;
+        else return;
+    }
+        
+}
+
